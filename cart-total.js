3421addf01a8553b7023a8c1ac10a2947c9125bb
DmitriiSerdiuk <102597936+DmitriiSerdiuk@users.noreply.github.com>
2023-04-02 23:38:27 +0300

    changed

--- cart-total.js
+++ cart-total.js
@@ -1,20 +1,24 @@
 // Задание №2
-function calculate(basketAmount, numbGoods, promotion = null) {
-    if (promotion === 'ДАРИМ300' && basketAmount < 300) {
-        console.log("Стоимость = 0");
-    } else if (promotion === 'ДАРИМ300') {
-        let totalAmount = basketAmount - 300;
-        console.log("Стоимость = " + totalAmount);
-    } else if (numbGoods >= 10) {
-        totalAmount = basketAmount / 100 * 5;
-        console.log("Стоимость = " + totalAmount);
-    } else if (basketAmount >= 50000) {
-        let basketDifference = basketAmount - 50000;
-        totalAmount = 50000 + (basketDifference - (basketDifference / 100 * 20));
-        console.log("Стоимость = " + totalAmount);
-    } else if (promotion === 'СКИДКА15' && basketAmount >= 20000) {
-        totalAmount = basketAmount / 100 * 15;
-        console.log("Стоимость = " + totalAmount);
+function calculate(totalSum, itemsCount, promoCode = null) {
+    let finalSum = totalSum;
+    if (promoCode === 'ДАРИМ300') {
+        finalSum = Math.max(finalSum - 300, 0);
     }
+
+    if (itemsCount >= 10) {
+        finalSum *= 0.95;
+    }
+
+    if (totalSum > 50000) {
+        const discount = (totalSum - 50000) * 0.2;
+        finalSum -= discount;
+    }
+
+    if (promoCode === 'СКИДКА15' && totalSum >= 20000) {
+        finalSum *= 0.85;
+    }
+
+    return finalSum;
 }
-export default { calculate }
+
+export default calculate;
--- filter-black-list.js
+++ filter-black-list.js
@@ -1,9 +1,8 @@
 var email = 'example@gmail.com';
 var blackList = ['serdiuk018@gmail.com', 'marlin8808@gmail.com', 'example@gmail.com', 'example2@gmail.com', 'example3@gmail.com', 'example4@gmail.com'];
 
-function filter() {
-    for (let i = 0; i < 1; ++i) {
-        console.log(blackList.includes(email) === true ? 'Емейл находится в черном списке' : 'Этого емейла нету в черном списке ');
-    }
+function filter(email, blackList) {
+    return blackList.includes(email) ? 'Емейл находится в черном списке' : 'Этого емейла нет в черном списке';
 }
-filter();
+
+export default filter;
